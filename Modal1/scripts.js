10e520c0c055332fde4d7022f69c9dbce186f38d
Luis Isla A <islaluis25@gmail.com>
2024-09-30 17:09:36 -0400

    MODAL1

--- Modal1/scripts.js
+++ Modal1/scripts.js
@@ -3,6 +3,10 @@ const openModalBtn = document.getElementById("open-modal-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
 const modal = document.getElementById("modal");
 
+const openModalBtnX = document.getElementById("open-modal-btn-x");
+const closeModalBtnX = document.getElementById("close-modal-btn-x");
+const modalX = document.getElementById("modalX");
+
 // Abrir el modal
 openModalBtn.addEventListener("click", function () {
   modal.style.display = "flex"; // Mostrar el modal
@@ -10,7 +14,16 @@ openModalBtn.addEventListener("click", function () {
 
 // Cerrar el modal cuando se hace clic en la 'X'
 closeModalBtn.addEventListener("click", function () {
-  modal.style.display = "none"; // Ocultar el modal
+  modal.style.display = "none"; // Ocultar el modals
+});
+// Abrir el modal
+openModalBtnX.addEventListener("click", function () {
+  modalX.style.display = "flex"; // Mostrar el modal
+});
+
+// Cerrar el modal cuando se hace clic en la 'X'
+closeModalBtnX.addEventListener("click", function () {
+  modalX.style.display = "none"; // Ocultar el modals
 });
 
 // Cerrar el modal si se hace clic fuera del contenido
